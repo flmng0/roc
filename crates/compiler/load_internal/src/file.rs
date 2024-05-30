a84c5691f994573e461cf1e68f387f915aaa969a
Agus Zubiaga <hi@aguz.me>
2024-05-30 20:11:51 -0300

    Add module param identifiers to solve's scope

--- crates/compiler/load_internal/src/file.rs
+++ crates/compiler/load_internal/src/file.rs
@@ -4611,6 +4611,7 @@ fn run_solve_solve(
         aliases,
         rigid_variables,
         abilities_store: pending_abilities,
+        params_pattern,
         ..
     } = module;
 
@@ -4658,6 +4659,7 @@ fn run_solve_solve(
             derived_module,
             #[cfg(debug_assertions)]
             checkmate,
+            params_pattern: params_pattern.map(|(_, _, pattern)| pattern.value),
         };
 
         let solve_output = roc_solve::module::run_solve(
